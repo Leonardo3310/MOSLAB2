5726528ff8ea061350f87791af7452b81b57a5b5
Pipeperez1305 <af.perezm1@uniandes.edu.co>
2024-09-09 13:20:21 -0500

    ej3-corregido

--- ejercicio3.py
+++ ejercicio3.py
@@ -20,6 +20,7 @@ communication_cost = {('L1','S1'): 48, ('L2','S1'): 38, ('L3','S1'): 24, ('L4','
                       ('L1','S2'): 49, ('L2','S2'): 33, ('L3','S2'): 47, ('L4','S2'): 31, ('L5','S2'): 11, ('L6','S2'):33, ('L7','S2'): 39,('L8','S2'): 47, ('L9','S2'): 11, ('L10','S2'): 30, ('L11','S2'): 42, ('L12','S2'): 21,
                       ('L1','S3'): 31, ('L2','S3'): 34, ('L3','S3'): 36, ('L4','S3'):37, ('L5','S3'): 25, ('L6','S3'): 24, ('L7','S3'): 12, ('L8','S3'): 46, ('L9','S3'): 16, ('L10','S3'):30,('L11','S3'): 18, ('L12','S3'): 48}  
 
+# Cobertura del sensor en las ubicaciones
 sensor_coverage = {
     ('S1', 'L1'): 1, ('S1', 'L2'): 1, 
     ('S1', 'L3'): 1, ('S1', 'L4'): 1,
@@ -41,21 +42,36 @@ sensor_coverage = {
     
 }
 
+# Adyacencias: Define si una ubicación es adyacente a otra (1 si lo es, 0 si no)
+adyacencias = {
+    ('L1', 'L2'): 1, ('L1', 'L3'): 1, ('L1', 'L5'): 1, ('L2', 'L1'): 1, ('L2', 'L5'): 1,
+    ('L3', 'L1'): 1, ('L3', 'L4'): 1, ('L3', 'L5'): 1, ('L3', 'L6'): 1, ('L3', 'L7'): 1, ('L3', 'L8'): 1,
+    ('L4', 'L3'): 1, ('L4', 'L6'): 1, ('L4', 'L11'): 1, ('L4', 'L5'): 1,
+    ('L5', 'L1'): 1, ('L5', 'L2'): 1, ('L5', 'L3'): 1, ('L5', 'L4'): 1, ('L5', 'L11'): 1, ('L5', 'L10'): 1,
+    ('L6', 'L3'): 1, ('L6', 'L4'): 1, ('L6', 'L8'): 1, ('L6', 'L11'): 1,
+    ('L7', 'L3'): 1, ('L7', 'L8'): 1, ('L7', 'L12'): 1,
+    ('L8', 'L3'): 1, ('L8', 'L6'): 1, ('L8', 'L7'): 1, ('L8', 'L12'): 1,('L8', 'L9'): 1,('L8', 'L11'): 1,
+    ('L9', 'L8'): 1, ('L9', 'L11'): 1, ('L9', 'L12'): 1,('L9', 'L10'): 1,
+    ('L10', 'L5'): 1, ('L10', 'L9'): 1, ('L10', 'L11'): 1,
+    ('L11', 'L4'): 1, ('L11', 'L5'): 1, ('L11', 'L6'): 1, ('L11', 'L8'): 1, ('L11', 'L9'): 1, ('L11', 'L10'): 1,
+    ('L12', 'L7'): 1, ('L12', 'L8'): 1, ('L12', 'L9'): 1
+}
+
 # Variables de decisión
 Model.x = Var(Model.S, Model.L, domain=Binary)
 
 # Función objetivo: Minimizar el costo total (instalación, energía, comunicación)
 def funcion_obj(Model):
-    return sum(instalation_cost[l] * Model.x[s, l] for s in Model.S for l in Model.L) + \
-           sum((energy_consumption[s] + communication_cost.get((s,l),0)) * Model.x[s, l] for s in Model.S for l in Model.L)
+    return sum(instalation_cost[l] * Model.x[s, l] for s in Model.S for l in Model.L) + sum((energy_consumption[s] + communication_cost.get((s,l),0)) * Model.x[s, l] for s in Model.S for l in Model.L)
 
 Model.obj = Objective(rule=funcion_obj, sense=minimize)
 
-# Restricción: Cada ubicación debe estar cubierta por al menos un sensor
-def coverage_rule(Model, l):
-    return sum(Model.x[s, l] * sensor_coverage[(s,l)] for s in Model.S) >= 1
+# Restricción: Cada ubicación debe estar cubierta por al menos un sensor o un sensor en una ubicación adyacente
+def regla_covertura(Model, l):
+    # Si un sensor cubre la ubicación o una adyacente
+    return sum(Model.x[s, l] * sensor_coverage.get((s, l), 0) for s in Model.S) + sum(Model.x[s, l_adj] * adyacencias.get((l_adj, l), 0) for s in Model.S for l_adj in Model.L) >= 1
 
-Model.coverage_constraint = Constraint(Model.L, rule=coverage_rule)
+Model.coverage_constraint = Constraint(Model.L, rule=regla_covertura)
 
 # Resolver el Modelo
 solver = SolverFactory('glpk')
